3784eb2a6b0d30b8178ee063c75db9e3b23da2f7
quentin <qhesry@gmail.com>
2019-04-17 15:07:50 +0200

    finish grid_buildings

--- src/main/groovy/grid_buildings.groovy
+++ src/main/groovy/grid_buildings.groovy
@@ -56,31 +56,8 @@ def processing() {
                         current_number_of_point = current_number_of_point + 1
                 }
         }
-        /*sql.execute("CREATE OR REPLACE FUNCTION VBEB_CLASSE2(wanted_len double precision) "
-                        +" RETURNS integer AS "
-                        +" DECLARE "
-                        +" current_fractional double precision := 0.0; "
-                        +" current_number_of_point integer := 1; "
-                        +" i record; "
-                        +" BEGIN "
-                        +" FOR i IN "
-                                +" SELECT id as id_column, st_transform(the_geom, 2154) as the_geom, st_length(st_transform(the_geom, 2154)) as line_length, build_id as build_id FROM receivers_build_ratio "
-                                +" LOOP "
-                                +" current_fractional := 0.0; "
-                                +" WHILE current_fractional <= (1.0)::double precision "
-                                +" LOOP " 
-                                        +" INSERT INTO indexed_points(old_edge_id, the_geom, number_on_line, gid) VALUES (i.id_column, ST_LineInterpolatePoint(i.the_geom, current_fractional), current_number_of_point, i.build_id);"
-                                        +" current_fractional := current_fractional + (wanted_len/ i.line_length); "
-                                        +" current_number_of_point := current_number_of_point + 1; "
-                                +" END LOOP; "
-                        +" END LOOP; "
-                        +" return i.id_column; "
-                        +" END; ")
-
-        sql.execute("select VBEB_CLASSE2(5);")
         
         sql.execute("ALTER TABLE indexed_points ADD COLUMN id SERIAL PRIMARY KEY;")
-        sql.execute("CREATE INDEX indexed_points_the_geom_gist ON indexed_points USING GIST (the_geom);")
 
         sql.execute("drop table if exists receivers_delete;")
         sql.execute("create table receivers_delete as SELECT r.ID, r.the_geom,r.gid build_id from indexed_points r, "+buildingTableName+" b where st_intersects(b.the_geom, r.the_geom);")
@@ -89,12 +66,9 @@ def processing() {
 
         sql.execute("alter table indexed_points DROP column id ;")
         sql.execute("alter table indexed_points add column id serial ;")
-        sql.execute("ALTER TABLE indexed_points ADD PRIMARY KEY (ID);")
 
         sql.execute("drop table if exists receivers;")
         sql.execute("create table receivers as select id id, gid gid, ST_Translate(ST_force3d(the_geom),0,0,4) the_geom from indexed_points;")
-        sql.execute("ALTER TABLE receivers ADD PRIMARY KEY (ID);")
-        sql.execute("CREATE INDEX receivers_the_geom_gist ON receivers USING GIST (the_geom);")*/
 
         literalOutput = i18n.tr("Process done !")
 }
